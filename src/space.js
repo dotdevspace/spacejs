fa360b5df97e9a8e487dbcb4564cbfcf6c871c4c
Luis Enrique Suárez <luissuarezdev@gmail.com>
2017-10-23 16:15:47 -0400

    space plugin

--- src/space.js
+++ src/space.js
@@ -1,15 +1,46 @@
 "use strict";
-var $SpaceIO = (function () {
-    function $SpaceIO($params) {
-    }
-    return $SpaceIO;
-}());
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var $SpaceJS = (function () {
     function $SpaceJS() {
     }
     $SpaceJS.prototype.$io = function ($config) {
         return new $SpaceIO($config);
     };
+    $SpaceJS.prototype.error = function ($description) {
+        console.error("SpaceJS \u00BB " + $description);
+    };
     return $SpaceJS;
 }());
+var $SpaceIO = (function (_super) {
+    __extends($SpaceIO, _super);
+    function $SpaceIO($params) {
+        if ($params === void 0) { $params = {}; }
+        var _this = _super.call(this) || this;
+        _this.$default = {
+            host: 'localhost',
+            port: 3000
+        };
+        _this.$config = {};
+        if (typeof io !== 'undefined') {
+            _this.$config = Object.assign(_this.$default, $params);
+        }
+        else {
+            _this.error('Socket.io is not defined');
+        }
+        return _this;
+    }
+    $SpaceIO.prototype.hello = function () {
+        console.info(this.$config);
+    };
+    return $SpaceIO;
+}($SpaceJS));
 var SpaceJS = new $SpaceJS();
--- src/space.ts
+++ src/space.ts
@@ -1,15 +1,31 @@
-class $SpaceIO {
-    constructor($params: object) {
-
-    }
-
-
-}
-
 class $SpaceJS {
 
     public $io($config: object) {
         return new $SpaceIO($config)
+    }
+
+    error($description) {
+        console.error(`SpaceJS » ${$description}`);
+    }
+
+}
+
+class $SpaceIO extends $SpaceJS {
+    $default: object = {
+        host: 'localhost',
+        port: 3000
+    };
+
+    $config: object = {};
+
+    constructor($params: object = {}) {
+        super();
+
+        if (typeof io !== 'undefined') {
+            this.$config = Object.assign(this.$default, $params);
+        } else {
+            this.error('Socket.io is not defined');
+        }
     }
 
 }
